dc0b440bdf994605f06ef0425131e2900ddde43d
Ivan Lim <ivalim@ymail.com>
2021-04-11 20:49:41 +0800

    working on product_edit

---
+++ JS/product_edit.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+   
+});
+
+function confirm_edit() {
+  var name = $('#item_name').val();
+  var desc = $('#item_description').val();
+  console.log(name);
+  console.log(desc);
+
+  if(confirm("Are you sure you want to edit?")){
+      alert("Editing item.");
+      return true;
+  }else{
+      return false;
+  }
+
+}
