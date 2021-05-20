e751bac7169be91b94f5c7af6bb3d9b3b5455f15
x045 <lolkyle2001@gmail.com>
2021-05-20 22:35:42 +0800

    Improved chat feature

--- public/JS/chat.js
+++ public/JS/chat.js
@@ -4,5 +4,30 @@ $(document).ready(function() {
 
         var message = $("#message").val();
         console.log(message);
+        
+        if(validateField(message)) {
+
+            var html = 
+                `<div class="d-flex justify-content-end mb-3">
+                     <div class="msg_container_you">
+                          ${message}
+                    </div>
+                    <i class="fa fa-user mt-3" aria-hidden="true"></i>
+                 </div>`;
+
+            //console.log(html);
+
+            $("#chat-box").append(html);
+
+            $("#message").val("");
+        }
     });
+
+    function validateField(content) {
+
+        if (content == "")
+            return false;
+        else
+            return true;
+    }
 });
